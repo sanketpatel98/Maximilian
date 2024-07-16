bfa007d6f5f5e8884142b0e2610ec40398459172
sanketpatel <patelsanketr98@gmail.com>
2024-07-16 09:55:00 -0400

    Gap added between pokelist components

--- pokemon/src/Components/PokemonList/PokemonList.jsx
+++ pokemon/src/Components/PokemonList/PokemonList.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import Pokemon from '../Pokemon/Pokemon';
-import dummyData from '../../assets/dummyData';
+import React from "react";
+import Pokemon from "../Pokemon/Pokemon";
+import dummyData from "../../assets/dummyData";
+import classes from "./PokemonList.module.css"
 
 export default function PokemonList() {
-    
   return (
     <div>
-        <div><h2>Pokemons</h2></div>
-        {dummyData && dummyData.map((data) => <Pokemon pokemon={data} key={data.id} />)}
-
+      <div>
+        <h2>Pokemons</h2>
+      </div>
+      <div className={classes.pokemonList}>
+        {dummyData &&
+          dummyData.map((data) => <Pokemon pokemon={data} key={data.id} />)}
+      </div>
     </div>
-  )
+  );
 }
