5acd6e9b0f48e2046d2a7472314a0028a36b2dba
sanketpatel <patelsanketr98@gmail.com>
2024-07-18 15:25:02 -0400

    Dummy data changed

--- src/assets/dummyData.js
+++ src/assets/dummyData.js
@@ -9,7 +9,12 @@ const pokemon = [
       { name: "Chlorophyll", color: "green", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-    weaknesses: ["Fire", "Psychic", "Flying", "Ice"],
+    weaknesses: [
+      { type: "Fire", color: "orange", contrastColor: "black" },
+      { type: "Psychic", color: "purple", contrastColor: "white" },
+      { type: "Flying", color: "gray", contrastColor: "black" },
+      { type: "Ice", color: "lightblue", contrastColor: "black" }
+    ],
     threats: [
       { name: "Charizard", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" },
       { name: "Alakazam", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/065.png" },
@@ -26,7 +31,11 @@ const pokemon = [
       { name: "Solar Power", color: "red", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-    weaknesses: ["Water", "Rock", "Ground"],
+    weaknesses: [
+      { type: "Water", color: "blue", contrastColor: "white" },
+      { type: "Rock", color: "gray", contrastColor: "black" },
+      { type: "Ground", color: "brown", contrastColor: "black" }
+    ],
     threats: [
       { name: "Blastoise", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png" },
       { name: "Onix", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/095.png" },
@@ -43,7 +52,10 @@ const pokemon = [
       { name: "Rain Dish", color: "blue", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-    weaknesses: ["Electric", "Grass"],
+    weaknesses: [
+      { type: "Electric", color: "yellow", contrastColor: "black" },
+      { type: "Grass", color: "green", contrastColor: "white" }
+    ],
     threats: [
       { name: "Raichu", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png" },
       { name: "Venusaur", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" },
@@ -60,7 +72,9 @@ const pokemon = [
       { name: "Lightning Rod", color: "yellow", contrastColor: "black" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-    weaknesses: ["Ground"],
+    weaknesses: [
+      { type: "Ground", color: "brown", contrastColor: "black" }
+    ],
     threats: [
       { name: "Golem", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/076.png" },
       { name: "Rhydon", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/112.png" },
@@ -78,7 +92,10 @@ const pokemon = [
       { name: "Friend Guard", color: "pink", contrastColor: "black" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png",
-    weaknesses: ["Steel", "Poison"],
+    weaknesses: [
+      { type: "Steel", color: "gray", contrastColor: "black" },
+      { type: "Poison", color: "purple", contrastColor: "white" }
+    ],
     threats: [
       { name: "Steelix", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/208.png" },
       { name: "Muk", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/089.png" },
@@ -96,7 +113,9 @@ const pokemon = [
       { name: "Unnerve", color: "gray", contrastColor: "black" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png",
-    weaknesses: ["Fighting"],
+    weaknesses: [
+      { type: "Fighting", color: "red", contrastColor: "white" }
+    ],
     threats: [
       { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" },
       { name: "Hitmonlee", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/106.png" },
@@ -114,7 +133,10 @@ const pokemon = [
       { name: "Swift Swim", color: "blue", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
-    weaknesses: ["Electric", "Grass"],
+    weaknesses: [
+      { type: "Electric", color: "yellow", contrastColor: "black" },
+      { type: "Grass", color: "green", contrastColor: "white" }
+    ],
     threats: [
       { name: "Raichu", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png" },
       { name: "Venusaur", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" },
@@ -132,7 +154,11 @@ const pokemon = [
       { name: "Steadfast", color: "red", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/066.png",
-    weaknesses: ["Psychic", "Flying", "Fairy"],
+    weaknesses: [
+      { type: "Psychic", color: "purple", contrastColor: "white" },
+      { type: "Flying", color: "gray", contrastColor: "black" },
+      { type: "Fairy", color: "pink", contrastColor: "black" }
+    ],
     threats: [
       { name: "Alakazam", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/065.png" },
       { name: "Pidgeot", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png" },
@@ -141,18 +167,27 @@ const pokemon = [
   },
   {
     id: 9,
-    name: "Gastly",
-    type: "Ghost/Poison",
-    level: 35,
+    name: "Geodude",
+    type: "Rock/Ground",
+    level: 22,
     abilities: [
-      { name: "Levitate", color: "purple", contrastColor: "white" }
+      { name: "Rock Head", color: "gray", contrastColor: "black" },
+      { name: "Sturdy", color: "gray", contrastColor: "black" },
+      { name: "Sand Veil", color: "gray", contrastColor: "black" }
     ],
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/092.png",
-    weaknesses: ["Ghost", "Psychic", "Dark"],
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png",
+    weaknesses: [
+      { type: "Water", color: "blue", contrastColor: "white" },
+      { type: "Grass", color: "green", contrastColor: "white" },
+      { type: "Ice", color: "lightblue", contrastColor: "black" },
+      { type: "Fighting", color: "red", contrastColor: "white" },
+      { type: "Ground", color: "brown", contrastColor: "black" },
+      { type: "Steel", color: "gray", contrastColor: "black" }
+    ],
     threats: [
-      { name: "Gengar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png" },
-      { name: "Mewtwo", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png" },
-      { name: "Umbreon", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png" }
+      { name: "Blastoise", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png" },
+      { name: "Venusaur", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" },
+      { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" }
     ]
   },
   {
@@ -161,12 +196,14 @@ const pokemon = [
     type: "Normal",
     level: 20,
     abilities: [
-      { name: "Run Away", color: "gray", contrastColor: "black" },
-      { name: "Adaptability", color: "gray", contrastColor: "black" },
-      { name: "Anticipation", color: "gray", contrastColor: "black" }
+      { name: "Run Away", color: "brown", contrastColor: "white" },
+      { name: "Adaptability", color: "brown", contrastColor: "white" },
+      { name: "Anticipation", color: "brown", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
-    weaknesses: ["Fighting"],
+    weaknesses: [
+      { type: "Fighting", color: "red", contrastColor: "white" }
+    ],
     threats: [
       { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" },
       { name: "Hariyama", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/297.png" },
@@ -184,7 +221,9 @@ const pokemon = [
       { name: "Gluttony", color: "gray", contrastColor: "black" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png",
-    weaknesses: ["Fighting"],
+    weaknesses: [
+      { type: "Fighting", color: "red", contrastColor: "white" }
+    ],
     threats: [
       { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" },
       { name: "Heracross", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/214.png" },
@@ -201,7 +240,10 @@ const pokemon = [
       { name: "Moxie", color: "blue", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/130.png",
-    weaknesses: ["Electric", "Rock"],
+    weaknesses: [
+      { type: "Electric", color: "yellow", contrastColor: "black" },
+      { type: "Rock", color: "gray", contrastColor: "black" }
+    ],
     threats: [
       { name: "Raikou", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/243.png" },
       { name: "Aerodactyl", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/142.png" },
@@ -218,7 +260,12 @@ const pokemon = [
       { name: "Multiscale", color: "purple", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png",
-    weaknesses: ["Ice", "Rock", "Dragon", "Fairy"],
+    weaknesses: [
+      { type: "Ice", color: "lightblue", contrastColor: "black" },
+      { type: "Rock", color: "gray", contrastColor: "black" },
+      { type: "Dragon", color: "purple", contrastColor: "white" },
+      { type: "Fairy", color: "pink", contrastColor: "black" }
+    ],
     threats: [
       { name: "Lapras", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/131.png" },
       { name: "Cloyster", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/091.png" },
@@ -235,7 +282,11 @@ const pokemon = [
       { name: "Unnerve", color: "purple", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png",
-    weaknesses: ["Bug", "Ghost", "Dark"],
+    weaknesses: [
+      { type: "Bug", color: "green", contrastColor: "black" },
+      { type: "Ghost", color: "purple", contrastColor: "white" },
+      { type: "Dark", color: "black", contrastColor: "white" }
+    ],
     threats: [
       { name: "Tyranitar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/248.png" },
       { name: "Scizor", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/212.png" },
@@ -252,11 +303,15 @@ const pokemon = [
       { name: "Rough Skin", color: "brown", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png",
-    weaknesses: ["Ice", "Dragon", "Fairy"],
+    weaknesses: [
+      { type: "Ice", color: "lightblue", contrastColor: "black" },
+      { type: "Dragon", color: "purple", contrastColor: "white" },
+      { type: "Fairy", color: "pink", contrastColor: "black" }
+    ],
     threats: [
       { name: "Weavile", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/461.png" },
       { name: "Salamence", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/373.png" },
-      { name: "Fairy Imposter Ditto", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/132.png" }
+      { name: "Ditto", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/132.png" }
     ]
   },
   {
@@ -270,82 +325,103 @@ const pokemon = [
       { name: "Justified", color: "red", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/448.png",
-    weaknesses: ["Fighting", "Ground", "Fire"],
+    weaknesses: [
+      { type: "Fighting", color: "red", contrastColor: "white" },
+      { type: "Ground", color: "brown", contrastColor: "black" },
+      { type: "Fire", color: "orange", contrastColor: "black" }
+    ],
     threats: [
-      { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" },
-      { name: "Gengar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png" },
-      { name: "Excadrill", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/530.png" }
+      { name: "Blaziken", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/257.png" },
+      { name: "Garchomp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png" },
+      { name: "Charizard", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" }
     ]
   },
   {
     id: 17,
     name: "Greninja",
     type: "Water/Dark",
-    level: 36,
+    level: 34,
     abilities: [
       { name: "Torrent", color: "blue", contrastColor: "white" },
-      { name: "Protean", color: "blue", contrastColor: "white" },
-      { name: "Battle Bond", color: "blue", contrastColor: "white" }
+      { name: "Protean", color: "blue", contrastColor: "white" }
     ],
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/658.png",
-    weaknesses: ["Fighting", "Bug", "Fairy"],
+    weaknesses: [
+      { type: "Electric", color: "yellow", contrastColor: "black" },
+      { type: "Fairy", color: "pink", contrastColor: "black" },
+      { type: "Bug", color: "green", contrastColor: "black" },
+      { type: "Fighting", color: "red", contrastColor: "white" }
+    ],
     threats: [
-      { name: "Machamp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png" },
-      { name: "Scizor", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/212.png" },
-      { name: "Togekiss", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/468.png" }
+      { name: "Jolteon", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png" },
+      { name: "Gardevoir", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/282.png" },
+      { name: "Heracross", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/214.png" }
     ]
   },
   {
     id: 18,
-    name: "Incineroar",
-    type: "Fire/Dark",
-    level: 38,
+    name: "Togekiss",
+    type: "Fairy/Flying",
+    level: 30,
     abilities: [
-      { name: "Blaze", color: "red", contrastColor: "white" },
-      { name: "Intimidate", color: "red", contrastColor: "white" }
+      { name: "Hustle", color: "pink", contrastColor: "black" },
+      { name: "Serene Grace", color: "pink", contrastColor: "black" },
+      { name: "Super Luck", color: "pink", contrastColor: "black" }
     ],
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/727.png",
-    weaknesses: ["Water", "Fighting", "Rock", "Ground"],
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/468.png",
+    weaknesses: [
+      { type: "Electric", color: "yellow", contrastColor: "black" },
+      { type: "Ice", color: "lightblue", contrastColor: "black" },
+      { type: "Poison", color: "purple", contrastColor: "white" },
+      { type: "Rock", color: "gray", contrastColor: "black" },
+      { type: "Steel", color: "gray", contrastColor: "black" }
+    ],
     threats: [
-      { name: "Primarina", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/730.png" },
-      { name: "Gyarados", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/130.png" },
-      { name: "Incineroar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/727.png" }
+      { name: "Raikou", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/243.png" },
+      { name: "Weavile", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/461.png" },
+      { name: "Metagross", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/376.png" }
     ]
   },
   {
     id: 19,
-    name: "Zeraora",
-    type: "Electric",
-    level: 38,
+    name: "Metagross",
+    type: "Steel/Psychic",
+    level: 40,
     abilities: [
-      { name: "Volt Absorb", color: "yellow", contrastColor: "black" },
-      { name: "Lightning Rod", color: "yellow", contrastColor: "black" },
-      { name: "Hidden Ability", color: "yellow", contrastColor: "black" }
+      { name: "Clear Body", color: "gray", contrastColor: "black" },
+      { name: "Light Metal", color: "gray", contrastColor: "black" }
     ],
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/807.png",
-    weaknesses: ["Ground"],
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/376.png",
+    weaknesses: [
+      { type: "Fire", color: "orange", contrastColor: "black" },
+      { type: "Ground", color: "brown", contrastColor: "black" },
+      { type: "Ghost", color: "purple", contrastColor: "white" },
+      { type: "Dark", color: "black", contrastColor: "white" }
+    ],
     threats: [
-      { name: "Landorus", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/645.png" },
+      { name: "Charizard", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" },
       { name: "Garchomp", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png" },
-      { name: "Excadrill", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/530.png" }
+      { name: "Gengar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png" }
     ]
   },
   {
     id: 20,
-    name: "Dragapult",
-    type: "Dragon/Ghost",
-    level: 40,
+    name: "Sylveon",
+    type: "Fairy",
+    level: 35,
     abilities: [
-      { name: "Clear Body", color: "purple", contrastColor: "white" },
-      { name: "Infiltrator", color: "purple", contrastColor: "white" },
-      { name: "Hidden Ability", color: "purple", contrastColor: "white" }
+      { name: "Cute Charm", color: "pink", contrastColor: "black" },
+      { name: "Pixilate", color: "pink", contrastColor: "black" }
     ],
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/887.png",
-    weaknesses: ["Ice", "Dragon", "Fairy", "Dark", "Ghost"],
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png",
+    weaknesses: [
+      { type: "Steel", color: "gray", contrastColor: "black" },
+      { type: "Poison", color: "purple", contrastColor: "white" }
+    ],
     threats: [
+      { name: "Metagross", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/376.png" },
       { name: "Gengar", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png" },
-      { name: "Hydreigon", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/635.png" },
-      { name: "Aegislash", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/681.png" }
+      { name: "Toxicroak", image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/454.png" }
     ]
   }
 ];
